bc57e28b3a9e2c6457204e59d3379d227ba03e41
Evgeniy121 <36643137+Evgeniy121@users.noreply.github.com>
2018-02-21 22:12:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+if (currency<=10000 ){
+  h=q=d=n=p=0;
+  while (currency-50>=0)
+  {currency=currency-50; h++;}
+  while (currency-25>=0)
+  {currency=currency-25; q++;}
+  while (currency-10>=0)
+  {currency=currency-10; d++;}
+  while (currency-5>=0)
+  {currency=currency-5; n++;}
+  while (currency-1>=0)
+  {currency=currency-1; p++;}
+  let user={
+    H :h,
+    Q :q,
+    D :d,
+    N :n,
+    P :p,
+  };
+  if (h==0)delete user.H;
+  if (q==0)delete user.Q;
+  if (d==0)delete user.D;
+  if (n==0)delete user.N;
+  if (p==0)delete user.P;
+
+ return(user); } else {
+   let user1={
+     error: "You are rich, my friend! We don't have so much coins for exchange"
+   } ;
+   return(user1);
+ }
 }
